952d80aaabbbd1822b3de941f33370e371931bc1
RedGear1234 <89768051+RedGear1234@users.noreply.github.com>
2023-04-08 01:37:55 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 const express = require("express")
 
 const app = express()
-console.log(app)
+
+
+app.get('https://63a415ae821953d4f2a8d860.mockapi.io/users', (req, res)=>{
+  console.log(req)
+})
 
 app.listen("3000", () =>{
   console.log(`listing on port 3000`)
